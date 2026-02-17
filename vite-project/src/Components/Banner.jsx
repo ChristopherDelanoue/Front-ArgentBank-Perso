71971da22f21e81dcb2b269b029b2ec9f313abb6
Christopher Delanoue <christopherdelanoue@macbook-pro-m1-pro-de-christopher.home>
2026-02-17 15:51:32 +0100

    avec le header banner et footer

---
+++ vite-project/src/Components/Banner.jsx
@@ -0,0 +1,13 @@
+function Banner() {
+    return (
+        <div className="Banner">
+            <img src="../../src/assets/Image/bank-tree.jpeg" alt='Arbre'></img>
+            <div className="Banner-description">
+                <h4>No fees.<br/> No Minimum deposit. <br/> Hight interest rates.</h4>
+                <p>Open a savings account with Argent Bank today!</p>
+            </div>
+        </div>
+    )
+}
+
+export default Banner;
--- vite-project/src/Pages/Home.jsx
+++ vite-project/src/Pages/Home.jsx
@@ -1,9 +1,12 @@
 import Header from '../Container/Header.jsx';
-
+import Banner from '../Components/Banner.jsx';
+import Footer from '../Components/Footer.jsx';
 function Home() {
     return (
         <div className='Home'>
             <Header />
+            <Banner />
+            <Footer />
         </div>
     )
 }
