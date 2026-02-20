155edcbaee5cdbd395cd2d1310938f3307d51edf
Christopher Delanoue <christopherdelanoue@MacBook-Pro-M1-Pro-de-Christopher.local>
2026-02-20 15:38:49 +0100

    front de base ok'

--- vite-project/src/Container/Home-Container.jsx
+++ vite-project/src/Container/Home-Container.jsx
@@ -1,6 +1,7 @@
 import Feature from '../Components/Feature.jsx';
+import Banner from '../Components/Banner.jsx';
 
-const features = [
+const homeContainer = [
     {"logo": "icon-chat.png", "title": "You are our #1 priority", "description": "Need to talk to a representative? You can get in touch through our"},
     {"logo": "icon-money.png", "title": "More savings means higher rates", "description": "The more you save with us, the higher your interest rate will be!"},
     {"logo": "icon-security.png", "title": "Security you can trust", "description": "We use top of the line encryption to make sure your data and money is always safe."}
@@ -8,10 +9,14 @@ const features = [
 
 function Features() {
     return (
-        <div className="features-container">
-            {features.map((feature, index) => (
+        <div className="Home-Container">
+            <Banner />
+            <div className="features-container">
+                {homeContainer.map((feature, index) => (
                     <Feature key={index} logo={feature.logo} title={feature.title} description={feature.description} />
-            ))}
+                ))}
+            </div>
+
         </div>
     )
 }
--- vite-project/src/Pages/Home.jsx
+++ vite-project/src/Pages/Home.jsx
@@ -1,12 +1,10 @@
 import Header from '../Container/Header.jsx';
-import Banner from '../Components/Banner.jsx';
 import Footer from '../Components/Footer.jsx';
-import Features from '../Container/Features.jsx';
+import Features from '../Container/Home-Container.jsx';
 function Home() {
     return (
         <div className='Home'>
             <Header />
-            <Banner />
             <Features />
             <Footer />
         </div>
