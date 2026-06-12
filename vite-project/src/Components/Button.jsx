a3c43863f78d06c993fb309936102607d181e07a
Christopher Delanoue <christopherdelanoue@macbook-pro-m1-pro-de-christopher.home>
2026-06-12 08:51:05 +0200

    dernier nettoyage avant soutenance

--- vite-project/src/Components/Button.jsx
+++ vite-project/src/Components/Button.jsx
@@ -1,9 +1,9 @@
-function Button({title, onclick}) {
-    return (
-        <div className="button-Container">
-            <button onClick={onclick} className="button" >{title}</button>
-        </div>
-    )
-}
+    function Button({title, onclick}) {
+        return (
+            <div className="button-Container">
+                <button onClick={onclick} className="button" >{title}</button>
+            </div>
+        )
+    }
 
-export default Button;
+    export default Button;
--- vite-project/src/Pages/SigninSlice.js
+++ vite-project/src/Pages/SigninSlice.js
@@ -5,24 +5,21 @@ const SigninSlice = createSlice({
     name: 'auth',
     initialState: {
         token: localStorage.getItem("token") || null,
-        isAuthenticated: !!localStorage.getItem("token"),
         user: null,
     },
     reducers: {
         setToken: (state, action) => {
             state.token = action.payload.token;
-            state.isAuthenticated = true;
             state.user = action.payload.user;
         },
         updateUserName: (state, action) => {
             if (state.user) {
-                state.user.userName = action.payload; // action.payload sera juste le nouveau texte
+                state.user.userName = action.payload;
             }
         },
         logout: (state) => {
             state.token = null;
             state.user = null;
-            state.isAuthenticated = false;
             localStorage.removeItem("token");
         }
     }
